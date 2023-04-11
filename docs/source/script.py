136fdcea3c52a5dbd440f992a043e713add1a58d
Yash Arora <yasharora102@gmail.com>
2023-04-11 21:14:38 +0530

    Added requirements for RTD

--- docs/source/script.py
+++
@@ -1,9 +0,0 @@
-import os
-from m2r import parse_from_file
-
-filename = '../../README.md'
-with open(filename, 'r') as f:
-    rst_content = parse_from_file(f)
-
-with open('README.rst', 'w') as f:
-    f.write(rst_content)
